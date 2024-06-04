978728a2d99e3be9f5ea9b7a680e7be37d2074bd
Wilbert Kenneth <wilbertkennetha@gmail.com>
2024-06-04 12:02:21 +0700

    landing page fix

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,25 +10,31 @@ use Inertia\Inertia;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [pageController::class, 'home'])->name('home');
+Route::get('/pharmacy', [pageController::class, 'pharmacy'])->name('pharmacy');
+Route::get('/appointment', [pageController::class, 'appointment'])->name('appointment');
+
+Route::get('/faq', [pageController::class, 'faq'])->name('faq');
+Route::post('/store', [pageController::class, 'store'])->name('store');
+
 
 Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+    return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/doctor/{doctors}', [pageController::class, 'show'])->name('doctor');
+    Route::get('/history', [pageController::class, 'history'])->name('history');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
